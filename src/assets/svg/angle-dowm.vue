b8bd1172e9b86707de292a94692990073ee34563
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-09-20 12:56:37 -0600

    Iconos usados en la seccion de busqueda

---
+++ src/assets/svg/angle-dowm.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg class="icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+    </svg>
+</template>
+
+<style scoped>
+
+.icon{
+    color: #616161;
+    background-color: #fff;
+    border-radius: 100%;
+    margin-left: 10px;
+}
+
+</style>
