7215b5ada0220b6b99ab3283c40bf6aaef8666e0
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-09-20 16:23:22 -0600

    Estilo de la card, junto con el metodo para poner la primera letra del nombre en mayuscula

---
+++ src/components/card.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="pokemon-card">
+    <img :src="pokemon.image" :alt="pokemon.name" class="pokemon-image">
+    <div class="pokemon-info">
+      <div class="pokemon-id"># {{ pokemon.id.toString().padStart(3, '0') }}</div>
+      <div class="pokemon-name">{{ capitalize(pokemon.name) }}</div>
+      <div class="pokemon-types">
+        <span v-for="type in pokemon.types" :key="type" class="pokemon-type">{{ type }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  pokemon: {
+    type: Object,
+    required: true
+  }
+});
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+</script>
+
+<style scoped>
+.pokemon-card {
+  width: 200px;
+  cursor: pointer;
+}
+
+.pokemon-card:hover {
+  transform: scale(1.03);
+}
+
+.pokemon-image {
+  width: 100%;
+  height: auto;
+  background-color: #F2F2F2;
+}
+
+.pokemon-info {
+  margin-top: 10px;
+}
+
+.pokemon-id {
+  font-size: 0.8em;
+  color: #919191;
+}
+
+.pokemon-name {
+  font-size: 1.2em;
+  font-weight: bold;
+  color: #333;
+}
+
+.pokemon-type {
+  display: inline-block;
+  background-color: #eee;
+  border-radius: 5px;
+  padding: 3px 5px;
+  margin: 2px;
+  font-size: 0.8em;
+  color: #555;
+}
+</style>
