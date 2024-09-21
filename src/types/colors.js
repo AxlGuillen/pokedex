5d87681bd2b370471736ff857d2df69acbfd5f3d
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-09-20 23:35:19 -0600

    Colores dependiendo de su tipo

---
+++ src/types/colors.js
@@ -0,0 +1,20 @@
+export const typesColors = {
+  bug: "#729F3F", // Bicho
+  dragon: ["#53A4CF", "#F16E57"], // Dragón
+  fairy: "#FDB9E9", // Hada
+  fire: "#FD7D24", // Fuego
+  ghost: "#7B62A3", // Fantasma
+  ground: ["#F7DE3F", "#AB9842"], // Tierra
+  normal: "#A4ACAF", // Normal
+  psychic: "#F2F2F2", // Psíquico
+  steel: "#9EB7B8", // Acero
+  dark: "#707070", // Siniestro
+  electric: "#EED535", // Eléctrico
+  fighting: "#D56723", // Lucha
+  flying: ["#3DC7EF", "#BDB8B7"], // Volador
+  grass: "#9BCC50", // Planta
+  ice: "#51C4E7", // Hielo
+  poison: "#B97FC9", // Veneno
+  rock: "#A38C21", // Roca
+  water: "#4592C4", // Agua
+};
