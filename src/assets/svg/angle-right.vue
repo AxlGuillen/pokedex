c31b439338ce572314efff99b707e9499cd28636
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-09-21 19:24:31 -0600

    Iconos para el header details

---
+++ src/assets/svg/angle-right.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg class="icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7"/>
+    </svg>
+</template>
+
+<style scoped>
+
+.icon{
+    color: #616161;
+    background-color: #fff;
+    border-radius: 100%;
+    margin-top: 6px;
+}
+
+</style>
